10a20f9cf07505a6779fb10534683e2db161275d
Teslassian <dvoneschwege@gmail.com>
2021-01-13 13:58:01 +0200

    Completed L6

--- lesson06/settings/project.Debug.cspy.ps1
+++ lesson06/settings/project.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson5\settings\project.Debug.general.xcl" --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson5\settings\project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson06\settings\project.Debug.general.xcl" --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson06\settings\project.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson5\settings\project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson5\settings\project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson06\settings\project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson06\settings\project.Debug.driver.xcl" 
 }
