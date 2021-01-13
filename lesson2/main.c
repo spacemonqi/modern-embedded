f01f19bbd9a16a30f3f12352e9b9d220966cbe77
Teslassian <dvoneschwege@gmail.com>
2021-01-13 09:18:33 +0200

    Completed L2

--- lesson2/main.c
+++ lesson2/main.c
@@ -1,11 +1,14 @@
 
 int main() {
-    unsigned int counter = 0;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
+    int counter = 0;
+    while (counter < 21) {      
+        ++counter;
+        if ((counter & 1) != 0) {
+            /* do something when the counter is odd */
+        }
+        else {
+            /* do something when the counter is even */
+        }
+    }   
     return 0;
 }
--- lesson2/settings/project.Debug.cspy.ps1
+++ lesson2/settings/project.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\embedded_programming\lesson1\settings\project.Debug.general.xcl" --backend -f "C:\Users\dvone\Desktop\Divigraph\embedded_programming\lesson1\settings\project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson2\settings\project.Debug.general.xcl" --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson2\settings\project.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\embedded_programming\lesson1\settings\project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\dvone\Desktop\Divigraph\embedded_programming\lesson1\settings\project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson2\settings\project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\dvone\Desktop\Divigraph\Embedded_Programming\lesson2\settings\project.Debug.driver.xcl" 
 }
